9b0560c69866dc007a9fc5acf6a392514ad2c327
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-10-22 01:04:54 +0300

    Feat : Titles Search History , Suggestions , Search Results for OnlineSearch

--- app/src/main/java/com/dd3boh/outertune/ui/screens/search/OnlineSearchScreen.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/search/OnlineSearchScreen.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,6 +26,7 @@ import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -35,7 +41,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -44,6 +52,7 @@ import androidx.navigation.NavController
 import com.dd3boh.outertune.LocalDatabase
 import com.dd3boh.outertune.LocalPlayerAwareWindowInsets
 import com.dd3boh.outertune.LocalPlayerConnection
+import com.dd3boh.outertune.R
 import com.dd3boh.outertune.constants.SuggestionItemHeight
 import com.dd3boh.outertune.extensions.toMediaItem
 import com.dd3boh.outertune.extensions.togglePlayPause
@@ -104,11 +113,24 @@ fun OnlineSearchScreen(
     }
 
     LazyColumn(
-        state = lazyListState
+        state = lazyListState,
+        contentPadding =
+        WindowInsets.systemBars
+            .only(WindowInsetsSides.Bottom)
+            .asPaddingValues(),
     ) {
+        item {
+            Text(
+                text = (stringResource(R.string.SearchHistory)),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+        }
+
         items(
             items = viewState.history,
-            key = { it.query }
+            key = { it.query },
         ) { history ->
             SuggestionItem(
                 query = history.query,
@@ -126,17 +148,28 @@ fun OnlineSearchScreen(
                     onQueryChange(
                         TextFieldValue(
                             text = history.query,
-                            selection = TextRange(history.query.length)
-                        )
+                            selection = TextRange(history.query.length),
+                        ),
                     )
                 },
                 modifier = Modifier.animateItemPlacement()
             )
         }
+        if (viewState.suggestions.isNotEmpty()) {
+            item {
+                Text(
+                    text = (stringResource(R.string.Sujestions)),
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+            }
+        }
+
 
         items(
             items = viewState.suggestions,
-            key = { it }
+            key = { it },
         ) { query ->
             SuggestionItem(
                 query = query,
@@ -149,17 +182,22 @@ fun OnlineSearchScreen(
                     onQueryChange(
                         TextFieldValue(
                             text = query,
-                            selection = TextRange(query.length)
-                        )
+                            selection = TextRange(query.length),
+                        ),
                     )
                 },
                 modifier = Modifier.animateItemPlacement()
             )
         }
 
-        if (viewState.items.isNotEmpty() && viewState.history.size + viewState.suggestions.size > 0) {
+        if (viewState.items.isNotEmpty()) {
             item {
-                HorizontalDivider()
+                Text(
+                    text = (stringResource(R.string.SearchResutls)),
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                )
             }
         }
 
